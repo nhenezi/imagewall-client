86f2e0a4715ee961b32ca5b7024504cb3c282fe5
Nikola Henezi <nikola.henezi@gmail.com>
2012-12-14 20:52:15 +0100

    getNew function added to collection

--- src/js/collections/pictures.js
+++ src/js/collections/pictures.js
@@ -9,6 +9,22 @@
    */
   app.collection.Pictures = Backbone.Collection.extend({
     model: app.model.Picture,
-    url: 'http://localhost/bcc/server/index.php/picture/getLatest/1/5',
+    url: 'http://localhost/bcc/server/index.php/picture/getLatest/1/1',
+
+    getLast: function() {
+      return _.last(this.models);
+    },
+
+    getNew: function() {
+      this.fetch({
+        url: 'http://localhost/bcc/server/index.php/picture/getLatest/' +
+          (this.getLast().get('id') || '1') + '/1',
+        update: true,
+        success: function(c) {
+          console.log(c);
+          app.collection.pictures.trigger('update', c);
+        }
+      });
+    },
   });
 })();
